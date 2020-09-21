35ea086dfdc136b39e2cdfff2b91739b1c16c151
HervalMata <hervalmataamparo@gmail.com>
2020-09-21 19:48:37 -0300

    Resource Category

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'category_name' => $this->category_name,
+            'slug' => $this->slug,
+            'description' => $this->description,
+            'active' => (bool)$this->active,
+            'photo_url' => $this->photo_url,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
