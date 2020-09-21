3b5f26d6f08902a6f702d3655452c7695a801d9f
HervalMata <hervalmataamparo@gmail.com>
2020-09-21 19:52:10 -0300

    Metodo index ShopCategoryController

--- app/Http/Controllers/Shop/CategoryController.php
+++ app/Http/Controllers/Shop/CategoryController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Shop;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        $categories = Category::all();
+        return CategoryResource::collection($categories);
     }
 
     /**
